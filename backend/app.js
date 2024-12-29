d4e8b853ed1c23d8f7eb560ea9c031036959bb49
Imed Ben Youssef <imed.benyoussef@gmail.com>
2024-12-29 19:49:16 +0100

    update MongoDB connection string in app.js; comment out backend service in docker-compose.yml

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb://localhost:27017/boycottDb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
